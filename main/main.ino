9d52062bf7291c18e35b37f897780ebde59e3a5d
Frederik Held <git@frederikheld.de>
2021-05-01 17:12:24 +0200

    Adds triggers and progress counter

--- main/main.ino
+++ main/main.ino
@@ -1,45 +1,67 @@
+/* CONFIG */
+
+// CO2 sensor:
+const int CO2_SENSOR_RANGE = 5000; // Messbereich des Sensors (steht im Datenblatt)
+const int CO2_PPM_THRESHOLD = 4000; // ab welchem PPM-Wert ist die Pflanze stark genug "beatmet"
+
+// Microphone:
+const int MIC_LOUDNESS_THRESHOLD = 1; // mic sensitivity
+const int MIC_TIME_THRESHOLD = 1000; // wie lang in der Vergangenheit gilt es noch als gesprochen
+const int MIC_READINGS_BUFFER_SIZE = 10; // über wie viele Messwerte wird gemittelt? Wird bei > 30 instabil!
+const int MIC_LOUDNESS_BUFFER_SIZE = 30;
+
 // Pins:
 const int PIN_MIC_IN = A0;
 const int PIN_CO2_IN = 4;
-
-// CO2 config:
-const int CO2_SENSOR_RANGE = 5000; // Messbereich des Sensors (steht im Datenblatt)
-
-// Microphone config:
-const int MIC_LOUDNESS_THRESHOLD = 10; // mic sensitivity
-const int MIC_TIME_THRESHOLD = 1000; // wie lang in der Vergangenheit gilt es noch als gesprochen
-const int MIC_READINGS_BUFFER_SIZE = 10; // über wie viele Messwerte wird gemittelt? Wird bei > 30 instabil!
 
 // Scheduler timings:
 const int CO2_READING_INTERVAL = 2000;
 const int MIC_READING_INTERVAL = 10;
 
-// Globals:
+
+/* GLOBALS */
+
+// Scheduler execution times:
 int schedulerCo2LastExecutionTime = millis();
 int schedulerMicLastExecutionTime = millis();
-int timeMillisLastHeard = millis();
 
+// Buffers:
 int micReadingsBuffer[MIC_READINGS_BUFFER_SIZE];
 int micReadingsBufferIndex = 0;
+int micLoudnessBuffer[MIC_LOUDNESS_BUFFER_SIZE];
+int micLoudnessBufferIndex = 0;
 
-// Functions:
-int getCO2PPMValue() {
+// Triggers:
+bool co2LevelIsReached = false;
+bool someoneIsSpeaking = false;
+
+// Progress:
+int progress = 0;
+
+
+/* FUNCTION DEFINITIONS */
+
+/**
+ * Returns the current CO2 value in PPM.
+ */
+int getCo2PPMValue() {
   
-  unsigned long CO2PulseLength = pulseIn(PIN_CO2_IN, HIGH, 2500000);
+  unsigned long co2PulseLength = pulseIn(PIN_CO2_IN, HIGH, 2500000);
 
-  float CO2Percent = (CO2PulseLength / 1000) / 1004.0;
+  float co2Percent = (co2PulseLength / 1000) / 1004.0;
 
-  int CO2PPM = CO2Percent * CO2_SENSOR_RANGE;
+  int co2PPM = co2Percent * CO2_SENSOR_RANGE;
 
-  return CO2PPM;
+  return co2PPM;
   
 }
 
-bool isSomeoneSpeaking() {
-  if (getMicLoudnessCompensated() > MIC_LOUDNESS_THRESHOLD) {
-    timeMillisLastHeard = millis();
-    return true;
-  } else if (timeMillisLastHeard > (millis() - MIC_TIME_THRESHOLD)) {
+/**
+ * Returns true if the CO2 level is above the
+ * configured CO2_PPM_THRESHOLD.
+ */
+bool isCo2LevelReached() {
+  if (getCo2PPMValue() > CO2_PPM_THRESHOLD) {
     return true;
   }
 
@@ -47,10 +69,10 @@ bool isSomeoneSpeaking() {
 }
 
 /**
- * This function returns the rolling average of
- * the given micValue to filter out noise.
+ * Returns the rolling average of the given micValue
+ * to filter out noise.
  */
-int bufferMicValue(int micValue) {
+int bufferMicReadings(int micValue) {
   
   // write value to buffer at current index:
   micReadingsBuffer[micReadingsBufferIndex] = micValue;
@@ -74,14 +96,56 @@ int bufferMicValue(int micValue) {
 }
 
 /**
- * This function compensates loudness for the drift in the
- * microphone hardware by calculating the loudness compared
- * to the rolling average of microphone readings.
+ * Returns the rolling average of the given micLoudness
+ * to filter out noise.
  */
-int getMicLoudnessCompensated() {
+int bufferMicLoudness(int micLoudness) {
+  
+  // write value to buffer at current index:
+  micLoudnessBuffer[micLoudnessBufferIndex] = micLoudness;
+
+  // move index (circular buffer: if index is out of buffer size, start at 0):
+  if (micLoudnessBufferIndex >= MIC_LOUDNESS_BUFFER_SIZE - 1) {
+    micLoudnessBufferIndex = 0;
+  } else {
+    micLoudnessBufferIndex++;
+  }
+
+  // calculate average over buffer:
+  int sum = 0;
+  for (int i = 0; i < MIC_LOUDNESS_BUFFER_SIZE; i++) {
+    sum += micLoudnessBuffer[i];
+  }
+
+  int average = sum / MIC_LOUDNESS_BUFFER_SIZE;
+
+  return average;
+}
+
+/**
+ * Returns the microphone loudness which is
+ *   * compensated for the drift in the microphone hardware
+ *   * buffered to filter out noise
+ */
+int getMicLoudness() {
   const int micValue = analogRead(PIN_MIC_IN);
-  int readingsAverage = bufferMicValue(micValue);
-  return abs(readingsAverage - micValue); 
+  int readingsAverage = bufferMicReadings(micValue);
+  int micLoudness = abs(readingsAverage - micValue);
+  
+  int micLoudnessAverage = bufferMicLoudness(micLoudness);
+  return micLoudnessAverage;
+}
+
+/**
+ * Returns true if the mic loudness is above the
+ * configured MIC_LOUDNESS_THRESHOLD.
+ */
+bool isSomeoneSpeaking() {
+  if (getMicLoudness() > MIC_LOUDNESS_THRESHOLD) {
+    return true;
+  }
+
+  return false;
 }
 
 void setup() {
@@ -95,6 +159,12 @@ void setup() {
     micReadingsBuffer[i] = 0;
   }
   micReadingsBufferIndex = 0;
+  
+  // initialize mic loudness buffer:
+  for (int i = 0; i < MIC_LOUDNESS_BUFFER_SIZE; i++) {
+    micLoudnessBuffer[i] = 0;
+  }
+  micLoudnessBufferIndex = 0;
 
 }
 
@@ -103,20 +173,24 @@ void loop() {
   // check if CO2 should be read:
   int currentTime = millis();
   if ((currentTime - schedulerCo2LastExecutionTime) > CO2_READING_INTERVAL) {
-    //Serial.println("READ CO2 <==");
-    //int co2PPM = getCO2PPMValue();
-    //Serial.println(co2PPM);
-    //Serial.println(10);
+    co2LevelIsReached = isCo2LevelReached();
+    
     schedulerCo2LastExecutionTime = millis();
   }
 
   // check if mic should be read:
   if ((currentTime - schedulerMicLastExecutionTime) > MIC_READING_INTERVAL) {
-    int micLoudnessCompensated = getMicLoudnessCompensated();
-    Serial.println(micLoudnessCompensated);
+    someoneIsSpeaking = isSomeoneSpeaking();
 
     schedulerMicLastExecutionTime = millis();
   }
+
+  // update progress:
+  if (co2LevelIsReached && someoneIsSpeaking) {
+    progress++;
+  }
+  Serial.println(progress);
+  
   
   delay(1);
 }
